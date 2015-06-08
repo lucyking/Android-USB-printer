10d3c05ccd8555dc27a20a6030ca062d534857e8
lucyking <lucyking@users.noreply.github.com>
2015-06-08 19:40:12 +0800

    str="string"
    a little modify

--- com/pradeep/usbprinter/MainActivity.java
+++ com/pradeep/usbprinter/MainActivity.java
@@ -16,7 +16,7 @@ public class PrintMessage extends Activity{
 	protected void onCreate(Bundle icicle) {
 		 super.onCreate(icicle);
 	     setContentView(R.layout.main);
-	     String msg = 'This is a test message';
+	     String msg = "This is a test message";
 	     PrintOrder printer = new PrintOrder();
 	     printer.Print(this,msg);
 	}
